ec3c3589de4f7a0bf6bd55a88e111c0c114fa01b
Steven Kang <buzzbumble@Stevens-MBP.hitronhub.home>
2019-03-17 16:41:43 -0700

    Games-I-Play added

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -15,4 +15,45 @@ $(document).ready(function() {
     $(this).toggleClass('active');
     $('.links').toggleClass('drop');
   })
+
+  $('.nav-item a').click(function() {
+    if ($(this).attr("href") != '#links') {
+      var id = $(this).attr("href");
+      id = id.substring(1, id.length);
+      var container = $(`.${id}-container`);
+      var active = $('.active-container');
+      $('.navbar h1 a').removeClass('inactive');
+      if (!container.hasClass('active-container')) {
+        active.addClass('vishidden')
+        active.one('transitionend', function(e) {
+          active.addClass('dispnone');
+          active.removeClass('active-container');
+          container.removeClass('dispnone');
+          setTimeout(function() {
+            container.removeClass('vishidden');
+          }, 50)
+          container.addClass('active-container');
+        })
+      }
+    }
+  })
+
+  $('.navbar h1 a').click(function() {
+    var id = $(this).attr('href');
+    id = id.substring(1, id.length);
+    console.log(`.$(id)-container`);
+    var active = $('.active-container');
+    var container = $('.header-container');
+    $(this).addClass('inactive')
+    active.addClass('vishidden');
+    active.one('transitionend', function(e) {
+      active.addClass('dispnone');
+      active.removeClass('active-container');
+      container.removeClass('dispnone');
+      setTimeout(function() {
+        container.removeClass('vishidden');
+      }, 50)
+      container.addClass('active-container');
+    })
+  })
 })
