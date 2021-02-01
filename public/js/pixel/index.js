77a087c0bdc40bdd16b98283e24b4be0dafc015e
Steven Kang <s.kang919@gmail.com>
2021-01-31 23:55:36 -0800

    Janky animations. Get rid of em and clean this shit up

--- public/js/pixel/index.js
+++ public/js/pixel/index.js
@@ -1,4 +1,26 @@
 $(document).ready(() => {
   let scene = $('#scene')[0];
-  let parallax = new Parallax(scene);
+  let parallax = new Parallax(scene, {
+    relativeInput: true,
+    limitX: 90,
+    limitY: 20,
+    frictionX: 0.075,
+    frictionY: 0.075,
+    scalarX: 6,
+    scalarY: 6,
+    clipRelativeInput: true
+  });
+
+  let mainBuilding = $('#menu-building');
+  mainBuilding.mouseenter(() => {
+    //parallax.disable();
+    $('#background, #buildings-far, #buildings-near').addClass("blurred");
+    parallax.scalar(2, 1);
+  });
+
+  mainBuilding.mouseleave(() => {
+    //parallax.enable();
+    $('#background, #buildings-far, #buildings-near').removeClass("blurred");
+    parallax.scalar(6, 6);
+  });
 });
